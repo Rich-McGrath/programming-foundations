3ee216a22bba2c8657a2c9b608af8ba028730ef2
Rich-McGrath <richmcgrath@gmail.com>
2016-09-04 19:21:23 -0400

    Completed Medium 2 example 4

--- lesson_3/medium2.md
+++ lesson_3/medium2.md
@@ -3,3 +3,7 @@
 2. Please see: munsters_name_age_gender_print_out.rb
 
 3. Please see: tricky_method.rb
+
+4. Please see: word_reverse.rb
+
+5. 
---
+++ lesson_3/word_reverse.rb
@@ -0,0 +1,10 @@
+sentence = 'Humpty Dumpty sat on a wall.'
+
+# Below would be a quick away to do this but not what was asked for.
+# puts sentence.reverse.to_s
+
+words = sentence.split(/\W/)
+words.reverse!
+backwards_sentence = words.join(' ') + '.'
+
+p backwards_sentence
