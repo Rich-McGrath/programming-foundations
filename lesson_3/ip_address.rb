0af33712b9e8e1bf89c2c2956c9e369204158699
Rich-McGrath <richmcgrath@gmail.com>
2016-09-06 17:04:20 -0400

    refactored code for valid ip exercise

--- lesson_3/ip_address.rb
+++ lesson_3/ip_address.rb
@@ -1,22 +1,16 @@
-def is_a_number?(character)
+def a_number?(character)
   to_integer = character.to_i
   to_integer.to_s == character
 end
 
-def is_valid_ip?(character)
-  character.length >= 4 && character.length <= 12
+def valid_ip?(character)
+  character.size >= 4 && character.size <= 12
 end
 
 def dot_separated_ip_address?(input_string)
-  dot_separated_words = input_string.split(".")
-  if !is_valid_ip?(dot_separated_words.join) || !is_a_number?(dot_separated_words.join)
-    return false
-  else
-    while dot_separated_words.size > 0 do
-      word = dot_separated_words.pop
-      return true
-    end
-  end
+  dot_separated_words = input_string.split('.')
+  joined_words = dot_separated_words.join
+  return false if !valid_ip?(joined_words) || !a_number?(joined_words)
 end
 
 puts 'Please enter in an IP address'
@@ -25,5 +19,5 @@ user_ip = gets.chomp
 if dot_separated_ip_address?(user_ip) == false
   puts "You entered #{user_ip} which is not a valid IP"
 else
-  puts "Hooray! You entered in a valid IP!"
+  puts "Hooray! You entered in a valid IP of #{user_ip}"
 end
