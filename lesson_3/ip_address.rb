1f7be3d2060fabb7266c847319b6796362111cec
Rich-McGrath <richmcgrath@gmail.com>
2016-09-07 16:52:57 -0400

    Refactored and fixed bugs

--- lesson_3/ip_address.rb
+++ lesson_3/ip_address.rb
@@ -1,24 +1,26 @@
-def a_number?(character)
+require 'pry'
+def user_input_a_number?(character)
   to_integer = character.to_i
   to_integer.to_s == character
 end
 
-def valid_ip?(character)
-  character.size == 3
-end
-
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split('.')
-  word = dot_separated_words.pop
-  joined_words = dot_separated_words.join
-  return false if !valid_ip?(word) || !a_number?(joined_words)
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false if word.size > 3
+    return false unless user_input_a_number?(word)
+  end
+  true
 end
 
 puts 'Please enter in an IP address'
 user_ip = gets.chomp
 
-if dot_separated_ip_address?(user_ip) == false
-  puts "You entered #{user_ip} which is not a valid IP"
-else
+if dot_separated_ip_address?(user_ip)
   puts "Hooray! You entered in a valid IP of #{user_ip}"
+else
+  puts "You entered #{user_ip} which is not a valid IP"
 end
