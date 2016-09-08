2d1475b0be61dee9d1ce0819968356c4eede0b43
Rich-McGrath <richmcgrath@gmail.com>
2016-09-08 16:38:23 -0400

    Parts 1 and 2 of the Tic Tac Toe game are completed

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = '0'.freeze
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "     |     |"
@@ -14,12 +24,30 @@ def display_board(brd)
   puts ""
 end
 
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ' '
+  loop do
+    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
 def intialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = 'X' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 board = intialize_board
+display_board(board)
 
+player_places_piece!(board)
+puts board.inspect
 display_board(board)
