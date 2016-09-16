fd82125b5895b68a814d658a17c279933bd9ca2f
Rich-McGrath <richmcgrath@gmail.com>
2016-09-16 18:52:32 -0400

    Refactored Code

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -3,11 +3,15 @@ require 'pry'
 INITIAL_MARKER = ' '.freeze
 PLAYER_MARKER = 'X'.freeze
 COMPUTER_MARKER = '0'.freeze
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/ABcSize
 def display_board(brd)
   system 'clear'
   puts "You're #{PLAYER_MARKER}'s and the computer is #{COMPUTER_MARKER}'s"
@@ -25,6 +29,7 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/MethodLength, Metrics/ABcSize
 
 def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
@@ -61,18 +66,25 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
-                  [[1, 5, 9], [3, 5, 7]]              # diagonal
-  winning_lines.each do |line|
-
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  # winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+  #                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
+  #                 [[1, 5, 9], [3, 5, 7]]              # diagonal
+  #   winning_lines.each do |line|
+  #     if brd[line[0]] == PLAYER_MARKER &&
+  #       brd[line[1]] == PLAYER_MARKER &&
+  #       brd[line[2]] == PLAYER_MARKER
+  #     return 'Player'
+  #     elsif brd[line[0]] == COMPUTER_MARKER &&
+  #       brd[line[1]] == COMPUTER_MARKER &&
+  #       brd[line[2]] == COMPUTER_MARKER
+  #     return 'Computer'
+  #     end
+  #   end
+  # The code below is the refactored version of the above code.
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
